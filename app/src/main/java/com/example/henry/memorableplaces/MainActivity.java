44e13b141a62a656c32e368732815245448fd622
Henry Wong <H_wong14@u.pacific.edu>
2019-07-02 15:29:08 -0700

    Added listview along with transition to Maps Activity

--- app/src/main/java/com/example/henry/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/henry/memorableplaces/MainActivity.java
@@ -4,21 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    public void goToMaps(View view){
-        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
-
-        startActivity(intent);
-
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView listView = findViewById(R.id.listView);
+
+        final ArrayList<String> locations = new ArrayList<String>();
+
+        locations.add("Add a new location!");
+
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, locations);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (locations.get(i).toString() == "Add a new location!") {
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
     }
 }
