254fe1dd488df8f647f27e0154733dac334ffebe
Henry Wong <H_wong14@u.pacific.edu>
2019-07-02 15:09:37 -0700

    Added Transition to Maps Activity

--- app/src/main/java/com/example/henry/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/henry/memorableplaces/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.henry.memorableplaces;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void goToMaps(View view){
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+
+        startActivity(intent);
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
     }
 }
